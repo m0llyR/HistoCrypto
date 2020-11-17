df03b6b4e298fb491a537fbb0d2f75a5a35fc264
MartinHvidberg <ovod13github>
2020-11-17 19:34:26 +0100

    more minor update of the test module

--- HCA/tests/test_Ceasar.py
+++ HCA/tests/test_Ceasar.py
@@ -4,13 +4,15 @@ from HCA import Caesar as c
 
 
 class TestCeasar(unittest.TestCase):
+
     def test_list_int(self):
-        """
-        Test Ceasar
-        """
+        """ Test Ceasar """
         data = "Pack my box with five dozen liquor jugs."
         result = c.encrypt(data, 6)
         self.assertEqual(result, "Vgiq se hud cozn lobk jufkt rowaux pamy.")
+        revers = c.decrypt(result, 6)
+        self.assertEqual(revers, data)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ README.md
@@ -0,0 +1,7 @@
+# HistoCrypto
+
+A collection of Python3 implementations of Historic Crypto algorithms
+
+**NOT to be used if you seriously wants to hide stuff!**
+
+best M0LLY
