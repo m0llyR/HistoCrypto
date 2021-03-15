dea94e9534d8946c52d03fe0bb0e9670a3979f01
Martin Hvidberg <Martin@Hvidberg.net>
2021-03-15 13:18:30 +0100

    pat_stat.py: Sequencer 0.1 = MWC working

---
+++ backoffice/pat_stat.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+""" pattern stat """
+
+fn = r"data/patters.txt"
+
+with open(fn, encoding="utf-8") as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+content = [x.strip() for x in content]
+cont = "".join(content)
+print(f"cont: {content}")
+print(f"cont: {cont}")
+
+num_min_seq = 3
+num_max_seq = 12
+
+dic_sequ = dict()
+for num_seqlen in range(num_min_seq, num_max_seq + 1):
+    print(f"Sequence: {num_seqlen}")
+    dic_sequ[num_seqlen] = dict()
+    for n in range(len(cont) - num_seqlen):
+        sequ = cont[n:n+num_seqlen]
+        # print(f".sequ: {sequ}")
+        if sequ not in dic_sequ[num_seqlen].keys():
+            dic_sequ[num_seqlen][sequ] = 1
+        else:
+            dic_sequ[num_seqlen][sequ] += 1
+
+for ka in dic_sequ.keys():
+    print(f"Sequence length: {ka}")
+    for kb in sorted(dic_sequ[ka].keys()):
+        if dic_sequ[ka][kb] > 1:
+            print(f"  seq: {kb}: {dic_sequ[ka][kb]}")
--- main.py
+++ main.py
@@ -3,15 +3,15 @@
 
 import HCA.Caesar
 
-print(''.join([chr(n) for n in range(65, 91)]))  # upper case = [65..90]
-print(''.join([chr(n) for n in range(97, 123)]))  # lower case = [97..122]
+# print(''.join([chr(n) for n in range(65, 91)]))  # upper case = [65..90]
+# print(''.join([chr(n) for n in range(97, 123)]))  # lower case = [97..122]
 
 rawt = "In there stepped a stately Raven of the saintly days of yore;"
 
 # Caesar
-s = 11
+s = 13
 enct = HCA.Caesar.encrypt(rawt, s)
 dect = HCA.Caesar.decrypt(enct, s)
-print("Original: {}\nCaesar  : {}\nDecrypt : {}".format(rawt, enct, dect))
+print(f"Original: {rawt}\nCaesar  : {enct}\nDecrypt : {dect}")
 
 # Vigenère
