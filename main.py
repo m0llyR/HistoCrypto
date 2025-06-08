8595ea4a801ed77efd50fa298c5cd4ef41d89ce8
martin <martin@hvidberg.net>
2025-06-08 13:09:40 +0200

    test new Github token SEQ20250608b

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 
 import HCA.Caesar
 
-print(''.join([chr(n) for n in range(65, 91)]))  # upper case = [65..90]
-print(''.join([chr(n) for n in range(97, 123)]))  # lower case = [97..122]
+# print(''.join([chr(n) for n in range(65, 91)]))  # upper case = [65..90]
+# print(''.join([chr(n) for n in range(97, 123)]))  # lower case = [97..122]
 
 rawt = "In there stepped a stately Raven of the saintly days of yore;"
 
